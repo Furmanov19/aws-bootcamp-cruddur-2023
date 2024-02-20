1fb93312d9ac54b4b720d45574330a7fa6953bf1
Artyom Furmanov <artyom.furmanov@ventionteams.com>
2024-02-20 12:04:48 +0000

    finish lambda

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,8 @@
 -- this file was manually created
-INSERT INTO public.users (display_name, handle, cognito_user_id)
+INSERT INTO public.users (display_name, handle, cognito_user_id, email)
 VALUES
-  ('Andrew Brown', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko' ,'MOCK');
+  ('Andrew Brown', 'andrewbrown' ,'MOCK', 'test@test.com'),
+  ('Andrew Bayko', 'bayko' ,'MOCK', 'test1@test.com');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
